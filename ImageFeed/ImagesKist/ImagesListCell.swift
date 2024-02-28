a3d8711cde770806d45ee0ecb8d4d02c3d11270e
Arthur <Fokusnik90999@gmail.com>
2024-02-28 15:20:58 +0500

    add images in tableView

---
+++ ImageFeed/ImagesKist/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Артур  Арсланов on 19.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+}
